9147f0484f10052b0c2aace3fb54fbe32bad8ffd
borgonha0774 <amiltonborgonha.exis@gmail.com>
2022-10-05 07:39:19 -0300

    create entities post and comment

---
+++ Entities/Post.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace OOPCompositionStringBuilder.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+
+        //Crindo o vinculo a composição e já garantir que sera iniciada
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public Post()
+        {
+        }
+
+        //não podera passar o argumento para muitos no construtor boas praticas
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+
+        //QUando temos uma composição tem muitos é comum colocar as operações adicionar e remover
+        public void AddComment (Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public void RemoveComment (Comment comment)
+        {
+            Comments.Remove(comment);
+        }
+    }
+}
